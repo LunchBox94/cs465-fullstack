be27d1be8633c5bc51c593734a755479fbae9941
Brennan Brickell <brennansf6@gmail.com>
2026-07-13 18:28:23 -0700

    Complete Module 2 MVC routing assignment

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var travlrController = require('../controllers/travlr');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET travel page. */
+router.get('/travel', travlrController.travel);
 
 module.exports = router;
