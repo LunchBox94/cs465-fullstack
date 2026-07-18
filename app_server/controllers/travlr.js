a0fa04798316ab5f89706feb36340b559c909f91
Brennan Brickell <brennansf6@gmail.com>
2026-07-17 21:45:12 -0700

    Complete Module Three assignment

--- app_server/controllers/travlr.js
+++ app_server/controllers/travlr.js
@@ -1,10 +1,24 @@
-/* GET Travel view */
+/* Load the modules needed for this controller */
+const fs = require('fs');
+const path = require('path');
+
+/* Read the trip information from the JSON file */
+const trips = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, '../data/trips.json'),
+    'utf8'
+  )
+);
+
+/* Display the Travel page with the trip data */
 const travel = (req, res) => {
-    res.render('travel', {
-        title: 'Travlr Getaways'
-    });
+  res.render('travel', {
+    title: 'Travlr Getaways',
+    trips
+  });
 };
 
+/* Make the travel controller available to other files */
 module.exports = {
-    travel
+  travel
 };
